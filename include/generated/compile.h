ef9fa16585a9a23cd83ab4aa85c976aaf0ac70d7
ISTweak <istweak@d-o-b.org>
2013-05-16 22:53:41 +0900

    ガバナー追加他

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 7 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Sat Jan 12 09:14:41 JST 2013"
-#define LINUX_COMPILE_TIME "09:14:41"
+#define UTS_VERSION "#7 PREEMPT Wed May 15 11:40:23 JST 2013"
+#define LINUX_COMPILE_TIME "11:40:23"
 #define LINUX_COMPILE_BY "love"
 #define LINUX_COMPILE_HOST "marijuana"
 #define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.6.3 (Sourcery CodeBench Lite 2012.03-56) "
+#define LINUX_COMPILER "gcc version 4.7.3 20130328 (prerelease) (Linaro GCC 4.7-2013.04) "
